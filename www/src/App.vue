be9ef022c82676b12d6a0282ced0f7451cc100d1
Eric Huff <thegreaterici@gmail.com>
2021-02-15 22:58:42 -0700

    Adds bootstrap for styling
    Reorganizes front end to look better while using bootstrap. Also adds
    some an additional data point to the return of the API for future
    rendering improvements.

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" class="container">
 		<Queue v-bind:queue="queue"/>
 	</div>
 </template>
@@ -14,7 +14,26 @@ export default {
 	},
 	data() {
 		return {
-			queue: [ ],
+			queue: [ 
+				{ 
+					id: '4efb9fdd-6b1b-468c-9a43-867e3a51c6cc', 
+					nickname: 'Abc',
+					time_joined: '11:01:32',
+					disabled: true,
+				},
+				{ 
+					id: '4e7533f9-28cc-4b4e-a534-2188a6fa1937', 
+					nickname: 'Glibbs',
+					time_joined: '11:01:32',
+					disabled: false,
+				},
+				{ 
+					id: 'a01e576e-06db-4184-b553-93af2b6d6eb8', 
+					nickname: 'Joen',
+					time_joined: '11:01:32',
+					disabled: false,
+				},
+			],
 		}
 	}
 }
