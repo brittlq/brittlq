be14847f8a1bb7500225795670ee81f4a163da07
PX Slip (Will K.) <dev@pxslip.com>
2021-09-30 16:43:55 +0000

    Adds tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
-import { faTwitch } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import App from './App.vue';
+import './assets/tailwind.css';
 
 library.add(faMinusCircle);
 library.add(faTwitch);
 
 createApp(App) /*.use(router)*/
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .mount("#app");
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
