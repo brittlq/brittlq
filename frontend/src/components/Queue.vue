8c318f755df2b0e80fd62591e52dee1e69246d15
PX Slip (Will K.) <dev@pxslip.com>
2021-10-14 18:56:02 +0000

    Trying to pull it all together in a clean UI

--- frontend/src/components/Queue.vue
+++ frontend/src/components/Queue.vue
@@ -2,89 +2,80 @@
   <div class="flex flex-row">
     <QueueControls
       :queue-length="queue.length"
-      @toggle_open="toggle_open"
+      @queuePop="queuePop"
       :is-open="isOpen"
     />
-    <div class="queue">
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Time</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <transition-group name="user-table">
-            <QueueEntry
-              v-for="(user, index) in queue"
-              :key="user.id"
-              :entry="user"
-              :index="index + 1"
-              @remove-user="remove"
-              class="queue-item"
-            ></QueueEntry>
-          </transition-group>
-        </tbody>
-      </table>
-    </div>
+    <table class="queue table-auto">
+      <thead>
+        <tr>
+          <th class="p-1">#</th>
+          <th class="p-1">Name</th>
+          <th class="p-1">Time</th>
+          <th class="p-1">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <QueueEntry
+          v-for="(user, index) in queue"
+          :key="user.id"
+          :entry="user"
+          :index="index + 1"
+          @remove-user="remove"
+          class="queue-item"
+        ></QueueEntry>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import QueueEntry from './QueueEntry.vue';
-import QueueControls from './QueueControls';
+import QueueControls from './QueueControls.vue';
 
 const axios = require('axios').default;
 
 export default {
   name: 'Queue',
   components: { QueueControls, QueueEntry },
-  created() {
-    //TODO: convert this to a websocket server? Avoids uneccessary network overhead
-    // this.poll(
-    //   () =>
-    //     new Promise(() =>
-    //       axios
-    //         .get('/queue')
-    //         .then((response) => {
-    //           return response.data;
-    //         })
-    //         .then((data) => {
-    //           this.queue = data.queue;
-    //           this.is_open = data.is_open;
-    //         })
-    //     ),
-    //   4000
-    // );
-  },
   data() {
-    return { isOpen: false, queue: [], isConnected: false };
+    return { isOpen: false, queue: [], isConnected: false, intervalId: null };
   },
+  created() {},
   mounted() {
     var hash_parameters = location.hash.substr(1);
-    var result = hash_parameters.split('&').reduce((res, item) => {
-      var parts = item.split('=');
-      res[parts[0]] = parts[1];
-      return res;
-    }, {});
-    axios
-      .post('/queue/token', JSON.stringify(result), {
-        headers: { 'content-type': 'application/json' },
-      })
-      .then((result) => {
-        console.log(result);
-        this.isConnected = true;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    if (hash_parameters.length > 0) {
+      var result = hash_parameters.split('&').reduce((res, item) => {
+        var parts = item.split('=');
+        res[parts[0]] = parts[1];
+        return res;
+      }, {});
+      axios
+        .post('/queue/token', JSON.stringify(result), {
+          headers: { 'content-type': 'application/json' },
+        })
+        .then((result) => {
+          console.log(result);
+          this.isConnected = true;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+    //TODO: convert this to a websocket server? Avoids uneccessary network overhead
+    this.intervalId = window.setInterval(this.poll, 4000);
+  },
+  unmounted() {
+    window.clearInterval(this.intervalId);
   },
   methods: {
-    poll(promiseFn, time) {
-      var sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
-      promiseFn().then(sleep(time).then(() => this.poll(promiseFn, time)));
+    async poll() {
+      try {
+        const { data } = await axios.get('/queue');
+        this.queue = data.queue;
+        this.is_open = data.is_open;
+      } catch (exc) {
+        console.error(exc);
+      }
     },
     remove(user) {
       if (user) {
@@ -98,27 +89,14 @@ export default {
         });
       }
     },
-    toggle_open(event) {
-      if (event) {
-        axios
-          .get('/queue/toggle')
-          .then((response) => {
-            return response.data;
-          })
-          .then((data) => {
-            console.log(data);
-            this.isOpen = data.isOpen;
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    },
     auth(event) {
       if (event) {
         let token = document.location.hash;
         console.log(token);
       }
+    },
+    queuePop(queue) {
+      this.queue = queue;
     },
   },
 };
