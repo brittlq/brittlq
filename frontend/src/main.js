c1051978093333eaa354a2ef8bcd2a6689857127
PX Slip (Will K.) <dev@pxslip.com>
2021-10-25 15:22:19 +0000

    Makes axios easily accesible

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,15 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 import { faTwitch } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import axios from 'axios';
 import App from './App.vue';
 import './assets/tailwind.css';
 
 library.add(faMinusCircle);
 library.add(faTwitch);
 
-createApp(App) /*.use(router)*/
+const app = createApp(App) /*.use(router)*/
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
+
+app.config.globalProperties.$axios = axios;
