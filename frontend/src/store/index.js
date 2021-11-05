fa3930b6073c1813ff6395335d08fb827ed692c7
PX Slip (Will K.) <dev@pxslip.com>
2021-11-05 13:26:11 +0000

    Start implementing the vuex store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,35 @@
 import { createStore } from 'vuex';
+import token from './get-token';
 
 export default createStore({
-  state: {},
+  state: {
+    token: token,
+    oauth: {
+      twitch: {
+        clientId: process.env.VUE_APP_TWITCH_CLIENT_ID,
+        redirectUri: process.env.VUE_APP_TWITCH_REDIRECT_URI,
+        claims: '{"id_token":{"email":null,"email_verified":null }}',
+        forceVerify: 'true',
+        scope: 'chat:read chat:edit',
+        responseType: 'token',
+      },
+    },
+  },
+  getters: {
+    twitchOauthUri: (state) => {
+      const url = new URL('/oauth2/authorize', 'https://id.twitch.tv');
+      url.searchParams.set('client_id', state.oauth.twitch.clientId);
+      url.searchParams.set('redirect_uri', state.oauth.twitch.redirectUri);
+      url.searchParams.set('response_type', state.oauth.twitch.responseType);
+      url.searchParams.set('scope', state.oauth.twitch.scope);
+      url.searchParams.set('force_verify', state.oauth.twitch.forceVerify);
+      url.searchParams.set(
+        'claims',
+        encodeURIComponent(state.oauth.twitch.claims)
+      );
+      return url.toString();
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
