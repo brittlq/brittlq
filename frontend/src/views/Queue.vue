ad9a1b2e4de800f6d0adf428d57aa84ee6134227
PX Slip (Will K.) <dev@pxslip.com>
2021-11-21 04:17:01 +0000

    Use the vue global version

--- frontend/src/views/Queue.vue
+++ frontend/src/views/Queue.vue
@@ -69,7 +69,10 @@ export default {
         console.log('Removing: ', user);
         var index = this.queue.indexOf(user.nickname);
         if (index >= 0) {
-          this.queue.splice(index, 1);
+          this.$axios.delete('/queue/' + user.nickname).then((response) => {
+            this.queue.splice(index, 1);
+            console.log('Confirmed removal of ', response);
+          });
         }
         this.$axios.delete('/queue/' + user.nickname).then((response) => {
           console.log('Confirmed removal of ', response);
