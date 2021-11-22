3bd4b52cd9586e01804ad169d04d1b437f84a814
PX Slip (Will K.) <dev@pxslip.com>
2021-11-22 06:10:16 +0000

    More OBS Work
    Move the logic into the vuex store
    Add a view to demo connecting, getting sources

---
+++ frontend/src/store/obs/mutations.js
@@ -0,0 +1,7 @@
+export const OBS_SET_PASSWORD = 'obs/OBS_SET_PASSWORD';
+export const OBS_SET_ADDRESS = 'obs/OBS_SET_ADDRESS';
+export const OBS_CONNECT = 'obs/OBS_CONNECT';
+export const OBS_SET_CONNECTION = 'obs/OBS_SET_CONNECTION';
+export const OBS_GET_SOURCES = 'obs/OBS_GET_SOURCES';
+export const OBS_SET_SOURCES = 'obs/OBS_SET_SOURCES';
+export const OBS_UPDATE_SOURCES = 'obs/OBS_UPDATE_SOURCES';
