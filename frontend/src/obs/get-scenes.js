505587f5da0c5409f77a5c647820971cd9d2f123
PX Slip (Will K.) <dev@pxslip.com>
2021-11-23 03:23:16 +0000

    Moving this functionality into vuex store

--- frontend/src/obs/get-scenes.js
+++
@@ -1,9 +0,0 @@
-import { connection } from '.';
-
-export default async () => {
-  try {
-    return await connection.send('GetSceneList');
-  } catch (exc) {
-    console.error(exc);
-  }
-};
--- frontend/src/obs/index.js
+++
@@ -1,9 +0,0 @@
-import connection from './connection';
-import getScenes from './get-scenes';
-import getSources from './get-sources';
-import getSourceSettings from './get-source-settings';
-import enableSource from './enable-source';
-
-export default connection;
-
-export { getScenes, getSources, getSourceSettings, enableSource };
