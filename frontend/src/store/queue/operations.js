d58f455681a707e443dd354519a988c4e3802cac
PX Slip (Will K.) <dev@pxslip.com>
2021-11-23 03:54:20 +0000

    Start pushing the queue data into vuex

---
+++ frontend/src/store/queue/operations.js
@@ -0,0 +1,5 @@
+const SET_TIME = 'queue/SET_TIME';
+const SET_PARTY_SIZE = 'queue/SET_PARTY_SIZE';
+const TOGGLE_OPEN = 'queue/TOGGLE_OPEN';
+const SET_DISABLED = 'queue/SET_DISABLED';
+const UPDATE = 'queue/UPDATE';
