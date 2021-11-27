106b44347d77c963f8bcc4936c4afd3f85962515
PX Slip (Will K.) <dev@pxslip.com>
2021-11-27 20:39:30 +0000

    One more component to name

--- frontend/src/components/layout/Navbar.vue
+++ frontend/src/components/layout/Navbar.vue
@@ -1,12 +1,6 @@
 <template>
   <nav
-    class="
-      flex flex-row
-      justify-items-center
-      col-span-4
-      border-b-2 border-gray-900
-      bg-gray-200
-    "
+    class="flex flex-row justify-items-center col-span-4 border-b-2 border-gray-900 bg-gray-200"
   >
     <h1 class="py-2 px-4 border-r border-gray-900 text-xl font-mono w-1/6">
       BrittlBot/BrittlQ
@@ -41,15 +35,7 @@
         <fa-icon v-else :icon="['fas', 'chevron-down']" />
       </MenuButton>
       <MenuItems
-        class="
-          absolute
-          right-0
-          origin-top-right
-          divide-y divide-gray-100
-          bg-gray-200
-          border-gray-900 border border-t-0
-          z-50
-        "
+        class="absolute right-0 origin-top-right divide-y divide-gray-100 bg-gray-200 border-gray-900 border border-t-0 z-50"
       >
         <MenuItem v-slot="{ active }" v-if="!hasToken">
           <a
@@ -112,6 +98,7 @@ import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
 import { storeToRefs } from 'pinia';
 import { computed, defineComponent } from 'vue';
 export default defineComponent({
+  name: 'BQNavbar',
   components: {
     Menu,
     MenuButton,
