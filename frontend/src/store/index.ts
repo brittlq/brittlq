22b7987c97f6dd3b85a90088c02f73e013dc283b
PX Slip (Will K.) <dev@pxslip.com>
2021-11-28 04:35:20 +0000

    Missed an import removal

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,7 @@
 export * from './axios';
 export { useObsStore, State as OBSState } from './obs';
 export { useQueueStore } from './queue';
-export { useTwitchStore, State as TwitchState } from './twitch';
+export { useTwitchStore as TwitchState } from './twitch';
 export { useTwitchChatStore } from './twitch-chat';
 
 export type UndefinedString = string | undefined;
