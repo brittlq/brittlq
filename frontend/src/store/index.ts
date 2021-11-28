a7396eaaab762d146d80bfff0d75168f0fa2da63
PX Slip (Will K.) <dev@pxslip.com>
2021-11-28 05:29:32 +0000

    Removing the explicit state typings

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,7 @@
 export * from './axios';
-export { useObsStore, State as OBSState } from './obs';
+export { useObsStore } from './obs';
 export { useQueueStore } from './queue';
-export { useTwitchStore as TwitchState } from './twitch';
+export { useTwitchStore } from './twitch';
 export { useTwitchChatStore } from './twitch-chat';
 
 export type UndefinedString = string | undefined;
